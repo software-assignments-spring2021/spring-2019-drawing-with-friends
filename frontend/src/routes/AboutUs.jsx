89b5a8e4ad02e0a9d938358f3318e018c5ea4482
David Zheng <dz1358@nyu.edu>
2019-03-05 01:45:57 -0500

    About Us Page

--- frontend/src/routes/AboutUs.jsx
+++ frontend/src/routes/AboutUs.jsx
@@ -3,5 +3,13 @@ import React from 'react'
 export default class AboutUs extends React.Component {
   render () {
     return <h1>About Us!</h1>
+           <p>Drawing With Friends, headquarted in New York, New York, focuses on providing entertaining while simoutaneously 
+	   challenging the creative mind. By offering a safe haven to interact with friends, tools for the imagination to
+           run wild, and challanges that test knowledge, Drawing With Friends offers a quality soluion for entertainment 
+           and socialization.</p>
+
+           <p>Drawing With Friends was founded in the Feburary of 2019 by a professor of Computer Science. The resulting
+           combination brought together a bunch of innovative minds with years of experience in the field of Computer
+           Science, forming the best source of entertaining on the web.</p>
   }
 }
