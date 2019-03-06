689e06286a3a6c0216070df82313ea66bdd2a2a4
Eric Lin <eric.lin@nyu.edu>
2019-03-06 16:35:08 -0500

    socketio barebone working

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
-const server = require('socket.io');
-const rooms = [];
+// Imports from libraries
+const socketio = require('socket.io');
 
+// Javascript Node variables.
+let server;
+let rooms = [];
+
+// Start listening for connections on current host
+server = socketio.listen(3000);
+console.log("Server is now listening on port 3000");
+
+// Server actions on receiving events
 server.on('connection', function(client) {
   // Debug message to log on connection
   client.emit('log', "Connection established to server");
@@ -8,6 +17,3 @@ server.on('connection', function(client) {
   // Option for client to create a room
   client.on('create room')
 });
-
-server.listen(3000);
-console.log("Server is now listening on port 3000");
