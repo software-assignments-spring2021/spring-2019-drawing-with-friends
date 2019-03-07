28aa139750517eb6133a332934df2f6fe61e063b
Eric Lin <eric.lin@nyu.edu>
2019-03-07 01:50:07 -0500

    Changing overflow

--- src/react/components/ChatBox.jsx
+++ src/react/components/ChatBox.jsx
@@ -5,21 +5,21 @@ export default class ChatBox extends React.Component {
     return (
       <div className="row">
         <center><h2>Chatbox</h2></center>
-        <div class="col s3"></div>
+        <div className="col s3"></div>
         <div className="chatbox col s6">
           <ul id="chatbox-messages"></ul>
           <form action="">
-            <div class="row">
-              <div class="input-field col s10">
-                <i class="material-icons prefix">send</i>
-                <input id="message" type="text" class="validate" autocomplete="off" />
+            <div className="row">
+              <div className="input-field col s10">
+                <i className="material-icons prefix">send</i>
+                <input id="message" type="text" autocomplete="off" />
                 <label for="message">Send your message</label>
               </div>
-              <button class="btn waves-effect waves-light col s2 chatbox-send" type="submit" name="action">Send</button>
+              <button className="btn waves-effect waves-light col s2 chatbox-send" type="submit" name="action">Send</button>
             </div>
           </form>
         </div>
-        <div class="col s3"></div>
+        <div className="col s3"></div>
       </div>
     )
   }
