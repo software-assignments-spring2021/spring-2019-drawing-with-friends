18c8ba171485b46ba99dc018bed02f9e74598d83
Rebecca Hom <rebecca.hom@nyu.edu>
2019-03-10 22:25:03 -0400

    Added React Canvas Component

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,17 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import HelloWorld from './components/HelloWorld.jsx'
-import Footer from './components/Footer.jsx'
-import NavigationBar from './components/NavigationBar.jsx'
+import Canvas from './components/Canvas/CanvasWrapper.jsx'
 
 class App extends React.Component {
   render () {
     return (
-      <>
-        <NavigationBar/>
-        <HelloWorld/>
-        <Footer/>
-      </>
+      <Canvas />
     )
   }
 }
