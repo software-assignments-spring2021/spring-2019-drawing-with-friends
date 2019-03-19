5edde13bbd37d1eb0007da994cbd5baa02b45ab7
Jephrey Liu <jephliu@jeph.io>
2019-03-19 13:50:55 -0400

    Setup backend testing

---
+++ backend/test/app.spec.js
@@ -0,0 +1,8 @@
+import { describe } from 'mocha'
+import { expect } from 'chai'
+
+describe('app.js', function () {
+  it('creates a socket io server', function () {
+    expect(true).to.equal(true)
+  })
+})
