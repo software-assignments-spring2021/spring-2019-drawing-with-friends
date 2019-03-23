a00c4ae18d22cfd39d21dec5b3a89bba2681bbff
Rebecca Hom <rebecca.hom@nyu.edu>
2019-03-23 14:34:59 -0400

    Added react router, fixed canvas

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import HelloWorld from './components/HelloWorld.jsx'
+import Footer from './components/Footer.jsx'
+import NavigationBar from './components/NavigationBar.jsx'
 import Canvas from './components/Canvas/CanvasWrapper.jsx'
 
 class App extends React.Component {
   render () {
     return (
-      <Canvas />
+      <React.Fragment>
+        <NavigationBar/>
+
+        <Switch>
+          <Route exact path="/game" component={Canvas}/>
+          <Route exact path="/" component={HelloWorld}/>
+        </Switch>
+
+        <Footer/>
+      </React.Fragment>
     )
   }
 }
 
-ReactDOM.render(<App/>, document.getElementById('main-container'))
+ReactDOM.render(
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>,
+  document.getElementById('main-container')
+)
