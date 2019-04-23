b6bc1d4eb2ff40d819449b7e501ddb007a77eccd
Jephrey Liu <jephliu@jeph.io>
2019-04-22 20:58:55 -0400

    Render chat messages

--- frontend/src/components/GamePage.jsx
+++ frontend/src/components/GamePage.jsx
@@ -3,6 +3,29 @@ import Canvas from './Canvas/CanvasWrapper.jsx'
 import '../css/GamePage.css'
 
 class GamePage extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      chatMessages: [
+        {
+          id: 1,
+          author: 'Jeph',
+          message: 'Test message :)'
+        }
+      ]
+    }
+  }
+
+  renderChatMessages () {
+    return this.state.chatMessages.map((message) => {
+      return (
+        <p key={message.id}>
+          {message.author}: {message.message}
+        </p>
+      )
+    })
+  }
+
   render () {
     return (
       <div className='gamePageContainer'>
@@ -17,6 +40,7 @@ class GamePage extends React.Component {
           <form>
             <textarea placeholder='Enter your answer!'/>
           </form>
+          {this.renderChatMessages()}
         </div>
       </div>
     )
