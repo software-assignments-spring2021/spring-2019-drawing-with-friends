73c0e1ea1d78716cd33eb6668ea8b145b54182d8
Rebecca Hom <rebecca.hom@nyu.edu>
2019-04-27 17:24:19 -0400

    Added Generate Unique code function

--- frontend/src/components/CreateGamePage.jsx
+++ frontend/src/components/CreateGamePage.jsx
@@ -2,16 +2,33 @@ import * as React from 'react'
 import '../css/CreateGamePage.css'
 
 class CreateGamePage extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      roomCode: this.generateCode()
+    }
+  }
+
   generateCode () {
-    // Insert real logic here
-    return 'ABC1'
+    let code = ''
+    for (let i = 0; i < 4; i++) {
+      // Decide if the character is a letter or number
+      const numOrLetter = Math.random() * 10
+      if (numOrLetter < 5) {
+        code += `${Math.floor(Math.random() * 10)}`
+      } else {
+        code += `${String.fromCharCode(Math.random() * (91 - 65) + 65)}`
+      }
+    }
+
+    return code
   }
 
   render () {
     return (
       <div className='createGameContainer'>
         <h2>Share this code with your friends!</h2>
-        <p>{this.generateCode()}</p>
+        <p>{this.state.roomCode}</p>
       </div>
     )
   }
