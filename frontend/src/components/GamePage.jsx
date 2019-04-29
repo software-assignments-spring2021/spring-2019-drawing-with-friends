466d23e2701883a3fd165da3bf13e9b625b93b10
Rebecca Hom <rebecca.hom@nyu.edu>
2019-04-29 00:23:25 -0400

    Added Instructional Modal

--- frontend/src/components/GamePage.jsx
+++ frontend/src/components/GamePage.jsx
@@ -4,15 +4,47 @@ import '../css/GamePage.css'
 import Chat from './Chat.jsx'
 
 class GamePage extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      isModalOpen: true
+    }
+
+    this.closeModal = this.closeModal.bind(this)
+  }
+
+  showModal () {
+    return (
+      <div className='instructionsModal'>
+        <div className='instructionsModalContent'>
+          <h4>{`What's This?`}</h4>
+          <p>This is the staging area where you can wait for your friends to join.</p>
+          <p>{`You can practice drawing on the canvas on this page and chat with your friends. 
+            When you're ready to start, press "Start Game"!`}</p>
+          <button onClick={this.closeModal}>Got it!</button>
+        </div>
+      </div>
+    )
+  }
+
+  closeModal () {
+    this.setState({
+      isModalOpen: false
+    })
+  }
+
   render () {
     return (
-      <div className='gamePageContainer'>
-        <h4>Share this code with your friends: {this.props.roomId}</h4>
-        <div className='canvasContainer'>
-          <Canvas socket={this.props.socket} />
+      <React.Fragment>
+        {this.state.isModalOpen ? this.showModal() : ''}
+        <div className='gamePageContainer'>
+          <h4>Share this code with your friends: {this.props.roomId}</h4>
+          <div className='canvasContainer'>
+            <Canvas socket={this.props.socket} />
+          </div>
+          <Chat socket={this.props.socket} playerName={this.props.playerName}/>
         </div>
-        <Chat socket={this.props.socket} playerName={this.props.playerName}/>
-      </div>
+      </React.Fragment>
     )
   }
 }
