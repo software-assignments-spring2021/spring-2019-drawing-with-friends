80bd3138ac031fbc84b8ba41f63a3c1b3d1f19ea
David Zheng <dz1358@nyu.edu>
2019-04-30 23:04:50 -0400

    Added different categories of words to word bank

---
+++ frontend/src/components/WordBank.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+let animal = ["cat", "dog", "eagle", "buffalo", "stingray", "racoon", "giraffe", "cheetah", "pony", "squirrel", "eel", "frog", "chicken", "wolf", "crocodile", "owl", "platypus", "hamster", "snake", "pig", "zebra", "whale", "cow", "bat", "porcupine", "ant"];
+
+let food = ["hot dog", "pizza", "burger", "drumstick", "tofu", "spaghetti", "broccoli", "egg", "fish", "sushi", "french fries", "steak", "chocolate", "ice cream", "kebab", "spring roll", "donut", "cheese", "mushroom", "cake", "bacon", "taco"];
+    
+let fruit = ["apple", "banana", "cherry", "grape", "strawberry", "dragonfruit", "pineapple", "orange", "watermelon", "lemon", "kiwi", "avocado", "durian", "cucumber", "tomato"];
+    
+let occupation = ["doctor", "teacher", "firefighter", "police", "farmer", "janitor", "athlete", "chef", "lifeguard", "president", "librarian", "sumo wrestler", "soldier", "babysitter"];
