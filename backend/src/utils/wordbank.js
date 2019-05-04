32bf6b5a3ae160a751c90921f7fbc4876b04defe
David Zheng <dz1358@nyu.edu>
2019-05-03 23:54:37 -0400

    Expanded wordbank

---
+++ backend/src/utils/wordbank.js
@@ -0,0 +1,15 @@
+export default function () {
+  return [
+    'cat', 'dog', 'eagle', 'buffalo', 'stingray', 'raccoon', 'giraffe', 'cheetah', 'pony', 'squirrel', 'eel',
+    'frog', 'chicken', 'wolf', 'crocodile', 'owl', 'platypus', 'hamster', 'snake', 'pig', 'zebra', 'whale',
+    'cow', 'bat', 'porcupine', 'ant', 'hot dog', 'pizza', 'burger', 'drumstick', 'tofu', 'spaghetti', 'broccoli',
+    'egg', 'fish', 'sushi', 'french fries', 'steak', 'chocolate', 'ice cream', 'kebab', 'spring roll', 'donut',
+    'cheese', 'mushroom', 'cake', 'bacon', 'taco', 'apple', 'banana', 'cherry', 'grape', 'strawberry', 'dragonfruit',
+    'pineapple', 'orange', 'watermelon', 'lemon', 'kiwi', 'avocado', 'durian', 'cucumber', 'tomato', 'doctor',
+    'teacher', 'firefighter', 'police', 'farmer', 'janitor', 'athlete', 'chef', 'lifeguard', 'president', 'librarian',
+    'sumo wrestler', 'soldier', 'babysitter', 'computer', 'mermaid', 'dragon', 'clown', 'lollipop', 'pacifier',
+    'fire', 'water', 'earth', 'air', 'crab', 'popsicle', 'river', 'baby', 'time', 'unicorn', 'zoo', 'music','eclipse',
+    'phone', 'lightning', 'maze', 'meteor', 'marshmallow', 'moon', 'rice', 'tiger', 'laser', 'dizzy', 'ghost',
+    'monkey', 'octopus', 'spider man', 'iron man', 'hulk', 'thanos', 'thor', 'pikachu'
+  ].sort(() => Math.random() - 0.5)
+}
