e9caf3ec86e59d65f20300205df29c88833f76fc
Jeph <jephliu@jeph.io>
2019-05-04 13:14:24 -0400

    Prevent buttons from disappearing on navbar and fix pencil being too large on safari issue

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import '../css/NavigationBar.css'
 
 export default class NavigationBar extends React.Component {
   render () {
@@ -7,8 +8,8 @@ export default class NavigationBar extends React.Component {
       <div className="navbar-fixed">
         <nav>
           <div className="nav-wrapper light-blue">
-            <Link to="/" style={{ marginLeft: '20px' }} className="brand-logo">Drawing with Friends</Link>
-            <ul className="right hide-on-med-and-down">
+            <Link to="/" style={{ marginLeft: '20px' }} className="title-logo">Drawing with Friends</Link>
+            <ul className="right">
               <li><Link to="/create" className="waves-effect waves-light btn">Create Room <i
                 className="material-icons right">color_lens</i></Link></li>
               <li><Link to="/join" className="waves-effect waves-light btn">Join Room <i
